dfa1959f85ab6bc2292a7347f504230901124109
Simao Nziaka <sdnziaka@gmail.com>
2019-08-17 13:47:02 -0600

    clean tests

--- src/spec/keylani.interface.test.js
+++ src/spec/keylani.interface.test.js
@@ -7,29 +7,6 @@ const {bind, map, getAllBindings, listen} = require('../keylani.interface');
 const Globals = require('../keylani.globals');
 const KeylaniDOMinterface = require('../keylani.dom');
 
-function fakeKeyBoardPress(key = "a", keyState = "keydown") {
-	let keyboardEvent = document.createEvent("KeyboardEvent");
-	let initMethod = typeof keyboardEvent.initKeyboardEvent !== 'undefined' ? "initKeyboardEvent" : "initKeyEvent";
-	let keys = {
-		"a": { keyCode: 65, charCode: 97 }
-	};
-
-	keyboardEvent[initMethod](
-		keyState, // event type: keydown, keyup, keypress
-		true,      // bubbles
-		true,      // cancelable
-		window,    // view: should be window
-		false,     // ctrlKey
-		false,     // altKey
-		false,     // shiftKey
-		false,     // metaKey
-		keys[key].keyCode,	// keyCode: unsigned long - the virtual key code, else 0
-		keys[key].charCode 	// charCode: unsigned long - the Unicode character associated with the depressed key, else 0
-	);
-
-	return document.dispatchEvent(keyboardEvent);
-}
-
 describe('Testing Keylani Interface', () => {
 
 	let getAllBindingsSpy;
@@ -153,6 +130,8 @@ describe('Testing Keylani Interface', () => {
 	});
 
 	it('should test map with an object with all props', () => {
+		spy(JSON, 'parse');
+
 		let mapSpy = spy(map);
 		let keyA = 'a';
 		let keyB = 'b';
@@ -183,6 +162,9 @@ describe('Testing Keylani Interface', () => {
 		expect('bind' in mapSpy.getCall(0).args[0][keyA]).to.be.true;
 		expect('label' in mapSpy.getCall(0).args[0][keyA]).to.be.true;
 		expect('when' in mapSpy.getCall(0).args[0][keyA]).to.be.true;
+
+		expect(JSON.parse.calledOnce).to.be.true;
+		JSON.parse.restore();
 	});
 
 	it('should have all keys added with map', () => {
@@ -268,34 +250,8 @@ describe('Testing Keylani Interface', () => {
 		expect(JSON.parse.calledOnce).to.be.true;
 		expect(KeylaniDOMinterface.__addLoudPanel.calledOnce).to.be.true;
 		expect(KeylaniDOMinterface.__listenDOM.calledOnce).to.be.true;
-	});
 
-	it('should verify the DOM for a few details (keylani paint class)', () => {
-		// let listenSpy = spy(listen);
-		// opts = {
-		// 	style: '',
-		// 	keyshow: true,
-		// 	loud: true,
-		// 	loudTimer: 5500,
-		// 	showLoudData: true
-		// };
-
-		// listenSpy(opts);
-
-		// fakeKeyBoardPress();
-
-		// // docuemnt.event.preventDefault();
-		// window.onkeydown = (e) => {
-		// 	e.stopPropagation();
-		// 	console.log(e);
-		// };
-		// // globalThis.event.stopPropagation();
-
-		// let loudElem = document.getElementsByClassName(Globals.__KEYLANI_SETTINGS__.loudClass);
-
-		// console.log(loudElem)
-
-		// expect(loudElem).to.not.be.undefined;
+		JSON.parse.restore();
 	});
 
 	it('should clean up the fake DOM', () => {
